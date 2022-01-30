1dc9e1d9cd8501e9ab4eb153ac0f7bceccd5e26a
Vitalii Peresoliak <vitalik21022@gmail.com>
2022-01-30 16:15:14 +0200

    complete task 1 and task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 
-
 const galleryElem = document.querySelector('.gallery');
-const bodyElem = document.querySelector('body');
 let modalImage;
 
 // 1.Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
@@ -16,12 +14,13 @@ const galleryMarkup = galleryItems.map(({ preview, original, description }) => `
       alt="${description}"
     />
   </a>
-</div>`).join('');
+</div>`).join("");
 
 galleryElem.insertAdjacentHTML('beforeend', galleryMarkup);
 
 
 // 2.Реалізація делегування на div.gallery і отримання url великого зображення.
+
 
 const onGalleryClick = event => {
     event.preventDefault();
@@ -36,20 +35,28 @@ galleryElem.addEventListener('click', onGalleryClick);
 
 // 3.Підключення скрипту і стилів бібліотеки модального вікна basicLightbox.
 
+
 const onCreateModal = img => basicLightbox.create(`
     <img src="${img}" width="1200" alt="${img}">
 `);
 
 
-// const onOpenModal = img => {
-//     modalImage = onCreateModal(img);
-//     modalImage.show();
-//     console.log("Open modal");
-// };
+// 4.Відкриття модального вікна по кліку на елементі галереї.
 
 
-// const onKeyPress = event => {
-//     if (event.code === "Escape") modalImage.close();
-//     console.log("Close modal with escape");
-//     document.removeEventListener("keyup", onKeyPress);
-// };
+const onOpenModal = img => {
+    modalImage = onCreateModal(img);
+    modalImage.show();
+  console.log("Open modal");
+  document.addEventListener("keyup", onKeyPress);
+};
+
+
+// 5.Закриття модального вікна після натискання клавіші Escape
+
+
+const onKeyPress = event => {
+    if (event.code === "Escape") modalImage.close();
+    console.log("Close modal with Escape");
+    document.removeEventListener("keyup", onKeyPress);
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryElem = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>`).join("");
+
+galleryElem.insertAdjacentHTML('beforeend', galleryMarkup);
+
+
+
+const lightboxOptions = new SimpleLightbox('.gallery a', {captions: true, captionsData: 'alt', captionDelay: 250});
